f4fd6c79b5e461587f75f6483211cb3abfcbcd02
AdityaGaur7 <adgaur027@gmail.com>
2024-12-29 23:16:42 +0530

    ResponseEntity - sending status code

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -8,29 +8,40 @@ import com.example.demo.service.ProductService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
 
 @RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class ProductController {
     @Autowired
 	ProductService productService;
 
 	@GetMapping("/products")
-	public List<Product> getProducts() {
-		return productService.getProducts();
+	public ResponseEntity<List<Product>> getProducts() {
+		return new ResponseEntity<>(productService.getProducts(),HttpStatus.OK);
 	}
 
 	@GetMapping("/products/{id}")
-	public Product getProductById(@PathVariable int id) {
-		return productService.getProductById(id);
+	public ResponseEntity<Product> getProductById(@PathVariable int id) {
+
+		Product product = productService.getProductById(id);
+		if(product != null) {
+			return new ResponseEntity<>(product,HttpStatus.OK);
+		}else
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
 	@PostMapping("/products")
